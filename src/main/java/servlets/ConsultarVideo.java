f62392646398028f0766d0a1a298cc593fb86a0b
rrosenzweig <rodrigo.rosenzweig@fing.edu.uy>
2019-11-17 17:25:21 -0300

    Se agrega funcionalidad de historial

--- src/main/java/servlets/ConsultarVideo.java
+++ src/main/java/servlets/ConsultarVideo.java
@@ -14,18 +14,21 @@ import javax.xml.rpc.ServiceException;
 import WS.WebServices;
 import WS.WebServicesService;
 import WS.WebServicesServiceLocator;
+import WS.DtLista;
 import WS.DtUsuario;
 import WS.DtVideo;
 
 @WebServlet("/ConsultarVideo")
 public class ConsultarVideo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ConsultarVideo() {
-        super();
-    }
+	private String nick;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ConsultarVideo() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		WebServicesService wsLocator = new WebServicesServiceLocator();
 		WebServices ws = null;
 		try {
@@ -36,42 +39,44 @@ public class ConsultarVideo extends HttpServlet {
 		}
 		String aux = request.getParameter("id");
 		DtVideo v = null;
-		if(aux != null) {
+		if (aux != null) {
 			try {
 				Integer i = Integer.valueOf(aux);
 				v = ws.findVideo(i.intValue());
-				if(v != null) {
+				if (v != null) {
 					request.setAttribute("video", v);
 					HttpSession se = request.getSession();
 					String login = (String) se.getAttribute("login");
-					if(login != null) {
+					if (login != null) {
 						int gustar = 0;
-						boolean propio=false;
-						String user = (String) se.getAttribute("nickname");	
+						boolean propio = false;
+						String user = (String) se.getAttribute("nickname");
+						nick = user;
+						this.agregarConsultaHistorial(request, response);
 						ws.seleccionarUsuario(user);
-						if(ws.seleccionarVideo(v.getNombre()) != null){
+						if (ws.seleccionarVideo(v.getNombre()) != null) {
 							propio = true;
 						}
-						if(v.getPrivado() && !propio) {
+						if (v.getPrivado() && !propio) {
 							request.setAttribute("mensaje", "ERROR al consultar el video.");
-							request.getRequestDispatcher("Index").forward(request, response);							
+							request.getRequestDispatcher("Index").forward(request, response);
 						}
 						ws.finCasoUso();
-						if(!v.getPrivado()){
+						if (!v.getPrivado()) {
 							String[] val = v.getValoracionesPositivas();
-							if(val != null) {
-								for(String s: val){
-									if(s.equals(user)){
-										gustar = 1; //el user le dio like al video
+							if (val != null) {
+								for (String s : val) {
+									if (s.equals(user)) {
+										gustar = 1; // el user le dio like al video
 									}
 								}
 							}
-							if(gustar == 0){
+							if (gustar == 0) {
 								val = v.getValoracionesNegativas();
-								if(val != null) {
-									for(String s: val){
-										if(s.equals(user)){
-											gustar = -1; //el user le dio dislike al video
+								if (val != null) {
+									for (String s : val) {
+										if (s.equals(user)) {
+											gustar = -1; // el user le dio dislike al video
 										}
 									}
 								}
@@ -80,27 +85,55 @@ public class ConsultarVideo extends HttpServlet {
 						request.setAttribute("gustar", gustar);
 						request.setAttribute("propio", propio);
 						request.getRequestDispatcher("consultarVideo.jsp").forward(request, response);
-					}else if(v.getPrivado()) {
+					} else if (v.getPrivado()) {
 						request.setAttribute("mensaje", "ERROR al consultar el video.");
 						request.getRequestDispatcher("Index").forward(request, response);
-					}else {
+					} else {
 						request.getRequestDispatcher("consultarVideo.jsp").forward(request, response);
 					}
 				}
 				request.setAttribute("mensaje", "ERROR al consultar el video.");
-				request.getRequestDispatcher("Index").forward(request, response);			
-			}catch(Exception e) {}
+				request.getRequestDispatcher("Index").forward(request, response);
+			} catch (Exception e) {
+			}
 		}
-		
-		
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private void agregarConsultaHistorial(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+			WebServicesService wsLocator = new WebServicesServiceLocator();
+			WebServices ws = null;
+			try {
+				ws = wsLocator.getWebServicesPort();
+			} catch (ServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			String aux = request.getParameter("id");
+			String duenioVideo = ws.findDuenioVideo(Integer.parseInt(aux));
+			DtVideo v = null;
+			v = ws.findVideo(Integer.parseInt(aux));
+			ws.seleccionarUsuario(duenioVideo);
+			ws.seleccionarVideo(v.getNombre());
+			ws.seleccionarUsuario(nick);
+			DtLista lst = ws.seleccionarLista("Historial");
+			ws.agregarVideo(v.getNombre(), lst);
+			ws.finCasoUso();
+		} catch (Exception e) {
+
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String aux = request.getParameter("publicos");
-		if(aux == null)
+		if (aux == null)
 			aux = request.getParameter("privados");
-		if(aux != null) {
+		if (aux != null) {
 			WebServicesService wsLocator = new WebServicesServiceLocator();
 			WebServices ws = null;
 			try {
@@ -114,37 +147,38 @@ public class ConsultarVideo extends HttpServlet {
 				Integer i = Integer.valueOf(aux);
 				v = ws.findVideo(i);
 				request.setAttribute("video", v);
-				if(v != null) {	
+				if (v != null) {
 					HttpSession se = request.getSession();
 					String login = (String) se.getAttribute("login");
-					if(login != null) {
+					if (login != null) {
 						int gustar = 0;
-						boolean propio=false;
-						String user = (String) se.getAttribute("nickname");	
+						boolean propio = false;
+						String user = (String) se.getAttribute("nickname");
 						ws.seleccionarUsuario(user);
-						if(ws.seleccionarVideo(v.getNombre()) != null){
+						if (ws.seleccionarVideo(v.getNombre()) != null) {
 							propio = true;
 						}
 						ws.finCasoUso();
-						if(!v.getPrivado()){
-							for(String s: v.getValoracionesPositivas()){
-								if(s.equals(user)){
-									gustar = 1; //el user le dio like al video
+						if (!v.getPrivado()) {
+							for (String s : v.getValoracionesPositivas()) {
+								if (s.equals(user)) {
+									gustar = 1; // el user le dio like al video
 								}
 							}
-							if(gustar == 0){
-								for(String s: v.getValoracionesNegativas()){
-									if(s.equals(user)){
-										gustar = -1; //el user le dio dislike al video
+							if (gustar == 0) {
+								for (String s : v.getValoracionesNegativas()) {
+									if (s.equals(user)) {
+										gustar = -1; // el user le dio dislike al video
 									}
-								}						
+								}
 							}
 						}
 						request.setAttribute("gustar", gustar);
 						request.setAttribute("propio", propio);
 					}
 				}
-			}catch(Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 		request.getRequestDispatcher("consultarVideo.jsp").forward(request, response);
 	}
